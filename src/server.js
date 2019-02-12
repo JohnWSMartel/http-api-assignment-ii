0c4000dfa53a616d577cbd4f9746f8ea93dc3164
JohnWSMartel <jwm3464@g.rit.edu>
2019-02-12 17:45:13 -0500

    Seeking out help pertaining to assignment directions

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const urlStruct = {
+/* const urlStruct = {
   '/': htmlHandler.getIndex,
   '/style.css': htmlHandler.getCSS,
   '/success': jsonHandler.success,
@@ -15,16 +15,13 @@ const urlStruct = {
   '/forbidden': jsonHandler.forbidden,
   '/notImplemented': jsonHandler.notImplemented,
   notFound: jsonHandler.notFound,
-};
+}; */
 
 const onRequest = (request, response) => {
   console.log(request.url);
   const parsedUrl = url.parse(request.url);
   const params = query.parse(parsedUrl.query);
 
-  htmlHandler.getIndex(request, response);
-  htmlHandler.getCSS(request, response);
-  
   switch (request.method) {
     case 'GET':
       if (parsedUrl.pathname === '/') {
@@ -33,25 +30,26 @@ const onRequest = (request, response) => {
         htmlHandler.getCSS(request, response);
       } else if (parsedUrl.pathname === '/getUsers') {
         jsonHandler.success(request, response);
-      } else if (parsedUrl.pathname === '/updateUser') {
-        jsonHandler.updateUser(request, response);
+      } else if (parsedUrl.pathname === '/notReal') {
+        jsonHandler.notFound(request, response);
       } else {
         jsonHandler.notFound(request, response);
       }
 
-      if (urlStruct[parsedUrl.pathname]) {
+      /* if (urlStruct[parsedUrl.pathname]) {
         urlStruct[parsedUrl.pathname](request, response, params);
       } else {
         urlStruct.notFound(request, response, params);
-      }
+      } */
       break;
     case 'HEAD':
       if (parsedUrl.pathname === '/getUsers') {
-        // if get users, send meta data back
-        jsonHandler.getUsersMeta(request, response);
-      } else {
+        jsonHandler.success(request, response);
+      } else if(parsedUrl.pathname === '/notReal'){
+		  jsonHandler.notFound(request, response); //needs to be without response
+	  } else {
         // if not found send 404 without body
-        jsonHandler.notFoundMeta(request, response);
+        jsonHandler.notFound(request, response);
       }
       break;
     default:
@@ -64,5 +62,3 @@ const onRequest = (request, response) => {
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
-
-/*  */
