f54beb52a400ce890d836d23177a16cb037d1493
JohnWSMartel <jwm3464@g.rit.edu>
2019-02-18 08:54:29 -0500

    Changes made with advice from class.

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,3 +1,5 @@
+const users = {};
+
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(object));
@@ -6,7 +8,7 @@ const respondJSON = (request, response, status, object) => {
 
 const success = (request, response) => {
   const responseJSON = {
-    message: 'This is a successful response.',
+    users,
   };
 
   respondJSON(request, response, 200, responseJSON);
@@ -72,8 +74,6 @@ const respondJSONMeta = (request, response, status) => {
   response.end();
 };
 
-const users = {};
-
 const getUsers = (request, response) => {
   const responseJSON = { users };
 
@@ -82,6 +82,33 @@ const getUsers = (request, response) => {
 
 const getUsersMeta = (request, response) => {
   respondJSONMeta(request, response, 200);
+};
+
+const addUser = (request, response, body) => {
+	const responseJSON = {message: 'Name and age are both required.',};
+	
+	if(!body.name || !body.age){
+		responseJSON.id = 'missingParams';
+		return respondJSON (request, response, 400, responseJSON);
+	}
+	
+	let responseCode = 201;
+	
+	if(users[body.name]){
+		responseCode = 204;
+	} else {
+		users[body.name] = {};
+	}
+	
+	users[body.name].name = body.name;
+	users[body.name].age = body.age;
+	
+	if(responseCode === 201){
+		responseJSON.message = 'Created Successfully';
+		return respondJSON(request, response, responseCode, responseJSON)
+	}
+	
+	return respondJSONMeta(request, response, responseCode);
 };
 
 
@@ -94,4 +121,5 @@ module.exports = {
   notFound,
   getUsers,
   getUsersMeta,
+  addUser,
 };
